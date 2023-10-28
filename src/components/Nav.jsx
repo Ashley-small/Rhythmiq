a1a8276ac7bd492b9b38bd53d56aac9ca49dcd8e
Ashley <Ashley_small@outlook.com>
2023-10-29 00:53:25 +0100

    change name

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { faMusic } from "@fortawesome/free-solid-svg-icons";
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
   return (
     <nav>
-      <h1>Soundwaves</h1>
+      <h1>Rhythmiq</h1>
       <button onClick={() => setLibraryStatus(!libraryStatus)}>
         Library
         <FontAwesomeIcon icon={faMusic} />
